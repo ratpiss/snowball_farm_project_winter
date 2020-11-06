40b327054398aa24cfd4a5a282a7681e7bd0e1c3
ratpiss <31166611+ratpiss@users.noreply.github.com>
2020-11-06 20:02:58 +0200

    Fix of not shutting down

--- pw_farm.py
+++ pw_farm.py
@@ -1,28 +1,31 @@
 from pydirectinput import mouseUp, mouseDown
 from pynput import keyboard
-from time import sleep, strftime
+from time import sleep
+import threading
 
 stop = False
 
 def on_press(key):
-    print(key)
-    if key == keyboard.Key.esc:
-        return False
+    if key == keyboard.Key.end:
+        return False #shutting down keyboard listener thread
 
-def farm():
+def farm(): #aiming and shooting
     mouseDown(duration=0, button='right')
-    sleep(0.1)
+    sleep(0.1) #aiming time 
     mouseDown(button='left', duration=0)
     mouseUp(button='left', duration=0)
     mouseUp(button='right', duration=0)
 
-sleep(5) 
+sleep(5) #pause to give you time for opening a game's window
+running = True #farming condition 
 
 with keyboard.Listener(on_press=on_press) as listener:
-    while :
+    while running:
         farm()
+        if threading.active_count() == 1: #Shutdown if there are only one thread left running
+            running = False  
     listener.join()
 
-
+#Releasing the keys after program is done
 mouseUp(button='left', duration=0)
 mouseUp(button='right', duration=0)
